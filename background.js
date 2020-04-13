09a1bdb7bed08c258f71d67ab995cccc2ce4edc6
vince <vince.siu4@gmail.com>
2020-04-12 22:52:02 -0700

    Pull alarm handlers out of sub-function and register them at top level
    According to documentation for the chrome background pages found here
    https://developer.chrome.com/extensions/background_pages, if listeners
    or handlers are not set at the top level, they will not be registered
    correctly.
    
    My intuition tells me that Javascript does some dank process isolation
    and listeners are probably reaped based on the lifetime of the parent
    process. If a handler is not set at the top level, then the lifetime
    of the handler is attached to the sub-function, which is ephemeral.
    
    From my observations, this change has caused the alarms to
    consistently be shown again.

--- background.js
+++ background.js
@@ -16,43 +16,44 @@ function add_alarm() {
     delayInMinutes: 5,
     periodInMinutes: 5
   })
-
-  chrome.alarms.onAlarm.addListener(function(alarm) {
-
-    if (alarm.name === MAIN_ALARM_NAME) {
-      chrome.runtime.getBackgroundPage(function(page) {
-        page.console.log("Alarm time is up, creating notification");
-      })
-      chrome.notifications.create({
-        message: 'It has been 30 minutes',
-        type: 'image',
-        iconUrl: chrome.runtime.getURL("images/take_a_break.png"),
-        imageUrl: chrome.runtime.getURL("images/take_a_break.png"),
-        title: 'Take a Break',
-        requireInteraction: true
-      });
-    };
-
-    if (alarm.name === REFRESH_ALARM_NAME) {
-      chrome.notifications.getAll(function(notifications) {
-        Object.keys(notifications).forEach(function(notification_id) {
-          console.log(notification_id);
-          chrome.notifications.update(
-            notification_id, {
-              message: 'It has been 30+ minutes...',
-              type: 'image',
-              iconUrl: chrome.runtime.getURL("images/take_a_break.png"),
-              imageUrl: chrome.runtime.getURL("images/take_a_break.png"),
-              title: '[Delayed] Take a Break',
-              requireInteraction: true
-            },
-          );
-        });
-      });
-    }
-  });
 }
 
 
 chrome.runtime.onInstalled.addListener(add_alarm);
 chrome.runtime.onStartup.addListener(add_alarm);
+
+
+chrome.alarms.onAlarm.addListener(function(alarm) {
+
+  if (alarm.name === MAIN_ALARM_NAME) {
+    chrome.runtime.getBackgroundPage(function(page) {
+      page.console.log("Alarm time is up, creating notification");
+    })
+    chrome.notifications.create({
+      message: 'It has been 30 minutes',
+      type: 'image',
+      iconUrl: chrome.runtime.getURL("images/take_a_break.png"),
+      imageUrl: chrome.runtime.getURL("images/take_a_break.png"),
+      title: 'Take a Break',
+      requireInteraction: true
+    });
+  };
+
+  if (alarm.name === REFRESH_ALARM_NAME) {
+    chrome.notifications.getAll(function(notifications) {
+      Object.keys(notifications).forEach(function(notification_id) {
+        console.log(notification_id);
+        chrome.notifications.update(
+          notification_id, {
+            message: 'It has been 30+ minutes...',
+            type: 'image',
+            iconUrl: chrome.runtime.getURL("images/take_a_break.png"),
+            imageUrl: chrome.runtime.getURL("images/take_a_break.png"),
+            title: '[Delayed] Take a Break',
+            requireInteraction: true
+          },
+        );
+      });
+    });
+  }
+});
